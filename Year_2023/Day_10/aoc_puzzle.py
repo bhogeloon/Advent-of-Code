05bd902802d43cdf1f0771168c98695f227eda58
Bert van Hogeloon <bert.vanhogeloon@routz.nl>
2024-02-26 17:01:11 +0100

    20240226

--- Year_2023/Day_10/aoc_puzzle.py
+++ Year_2023/Day_10/aoc_puzzle.py
@@ -11,6 +11,8 @@ import numpy as np
 
 # Constants
 
+MAX_ROUNDS = 25
+
 UUCODE_TABLE = {
     '|': '\u2502',
     '-': '\u2501',
@@ -114,6 +116,14 @@ class Pipe():
         '''Set neighbors status as either Inside or Outside'''
         if self.char == '|' or self.char == '-':
             self._set_neighbor_status_straight(grid)
+        elif self.char == 'L':
+            self._set_neighbor_status_ne(grid)
+        elif self.char == 'J':
+            self._set_neighbor_status_nw(grid)
+        elif self.char == '7':
+            self._set_neighbor_status_sw(grid)
+        elif self.char == 'F':
+            self._set_neighbor_status_se(grid)
 
 
     def _set_neighbor_status_straight(self, grid) -> None:
@@ -144,12 +154,247 @@ class Pipe():
         nb_y = self.y + 1
         self._set_nb_io(nb_x, nb_y, io, grid)
 
+        nb_x = self.x - 1
+        nb_y = self.y + 1
+        self._set_nb_io(nb_x, nb_y, io, grid)
+
+
+    def _set_neighbor_status_nw(self, grid) -> None:
+        '''in case of "J"'''
+        if self.ori['in_x'] == 1:
+            io = 'I'
+        else:
+            io = 'O'
+
+        nb_x = self.x + 1
+        nb_y = self.y
+        self._set_nb_io(nb_x, nb_y, io, grid)
+
+        nb_x = self.x
+        nb_y = self.y + 1
+        self._set_nb_io(nb_x, nb_y, io, grid)
+
+        nb_x = self.x + 1
+        nb_y = self.y + 1
+        self._set_nb_io(nb_x, nb_y, io, grid)
+
+
+    def _set_neighbor_status_sw(self, grid) -> None:
+        '''in case of "7"'''
+        if self.ori['in_x'] == 1:
+            io = 'I'
+        else:
+            io = 'O'
+
+        nb_x = self.x + 1
+        nb_y = self.y
+        self._set_nb_io(nb_x, nb_y, io, grid)
+
+        nb_x = self.x
+        nb_y = self.y - 1
+        self._set_nb_io(nb_x, nb_y, io, grid)
+
+        nb_x = self.x + 1
+        nb_y = self.y - 1
+        self._set_nb_io(nb_x, nb_y, io, grid)
+
+
+    def _set_neighbor_status_se(self, grid) -> None:
+        '''in case of "F"'''
+        if self.ori['in_x'] == -1:
+            io = 'I'
+        else:
+            io = 'O'
+
+        nb_x = self.x - 1
+        nb_y = self.y
+        self._set_nb_io(nb_x, nb_y, io, grid)
+
+        nb_x = self.x
+        nb_y = self.y - 1
+        self._set_nb_io(nb_x, nb_y, io, grid)
+
+        nb_x = self.x - 1
+        nb_y = self.y - 1
+        self._set_nb_io(nb_x, nb_y, io, grid)
+
 
     def _set_nb_io(self, nb_x, nb_y, io, grid) -> None:
         if nb_x >= 0 and nb_x < grid.x_size and \
             nb_y >= 0 and nb_y < grid.y_size:
             if grid.pipes[nb_x,nb_y].status == 'U':
                 grid.pipes[nb_x,nb_y].status = io
+                # print("setting status to {} for {},{}".format(io, nb_x, nb_y))
+
+
+    def set_ori(self, prev_pipe) -> None:
+        self.status = 'L'
+        # print('setting status to L for {},{}'.format(self.x, self.y))
+
+        if self.char == '-':
+            self._set_ori_hor(prev_pipe)
+        elif self.char == '|':
+            self._set_ori_ver(prev_pipe)
+        elif self.char == 'L':
+            self._set_ori_ne(prev_pipe)
+        elif self.char == 'J':
+            self._set_ori_nw(prev_pipe)
+        elif self.char == '7':
+            self._set_ori_sw(prev_pipe)
+        elif self.char == 'F':
+            self._set_ori_sw(prev_pipe)
+
+
+    def _set_ori_hor(self, prev_pipe) -> None:
+        if prev_pipe.ori['in_y'] == 1:
+            self.ori = {
+                'in_x': 0,
+                'in_y': 1,
+                'out_x': 0,
+                'out_y': -1,
+            }
+        else:
+            self.ori = {
+                'in_x': 0,
+                'in_y': -1,
+                'out_x': 0,
+                'out_y': 1,
+            }
+
+
+    def _set_ori_ver(self, prev_pipe) -> None:
+        if prev_pipe.ori['in_x'] == 1:
+            self.ori = {
+                'in_x': 1,
+                'in_y': 0,
+                'out_x': -1,
+                'out_y': 0,
+            }
+        else:
+            self.ori = {
+                'in_x': -1,
+                'in_y': 0,
+                'out_x': 1,
+                'out_y': 0,
+            }
+
+
+    def _set_ori_ne(self, prev_pipe) -> None:
+        '''L'''
+        if prev_pipe.y == self.y:
+            if prev_pipe.ori['in_y'] == 1:
+                left_under = 'in'
+            else:
+                left_under = 'out'
+        else:
+            if prev_pipe.ori['in_x'] == 1:
+                left_under = 'out'
+            else:
+                left_under = 'in'
+
+        if left_under == 'in':
+            self.ori = {
+                'in_x': -1,
+                'in_y': 1,
+                'out_x': 1,
+                'out_y': -1,
+            }
+        else:
+            self.ori = {
+                'in_x': 1,
+                'in_y': -1,
+                'out_x': -1,
+                'out_y': 1,
+            }
+            
+
+
+    def _set_ori_nw(self, prev_pipe) -> None:
+        '''J'''
+        if prev_pipe.y == self.y:
+            if prev_pipe.ori['in_y'] == 1:
+                right_under = 'in'
+            else:
+                right_under = 'out'
+        else:
+            if prev_pipe.ori['in_x'] == 1:
+                right_under = 'in'
+            else:
+                right_under = 'out'
+
+        if right_under == 'in':
+            self.ori = {
+                'in_x': 1,
+                'in_y': -1,
+                'out_x': -1,
+                'out_y': 1,
+            }
+        else:
+            self.ori = {
+                'in_x': -1,
+                'in_y': 1,
+                'out_x': 1,
+                'out_y': -1,
+            }
+
+
+    def _set_ori_sw(self, prev_pipe) -> None:
+        '''7'''
+        if prev_pipe.y == self.y:
+            if prev_pipe.ori['in_y'] == 1:
+                right_above = 'out'
+            else:
+                right_above = 'in'
+        else:
+            if prev_pipe.ori['in_x'] == 1:
+                right_above = 'in'
+            else:
+                right_above = 'out'
+
+        if right_above == 'in':
+            self.ori = {
+                'in_x': 1,
+                'in_y': -1,
+                'out_x': -1,
+                'out_y': 1,
+            }
+        else:
+            self.ori = {
+                'in_x': -1,
+                'in_y': 1,
+                'out_x': 1,
+                'out_y': -1,
+            }
+
+
+    def _set_ori_se(self, prev_pipe) -> None:
+        '''F'''
+        if prev_pipe.y == self.y:
+            if prev_pipe.ori['in_y'] == 1:
+                left_above = 'out'
+            else:
+                left_above = 'in'
+        else:
+            if prev_pipe.ori['in_x'] == 1:
+                left_above = 'out'
+            else:
+                left_above = 'in'
+
+        if left_above == 'in':
+            self.ori = {
+                'in_x': -1,
+                'in_y': -1,
+                'out_x': 1,
+                'out_y': 1,
+            }
+        else:
+            self.ori = {
+                'in_x': 1,
+                'in_y': 1,
+                'out_x': -1,
+                'out_y': -1,
+            }
+
 
 
 class Grid():
@@ -173,8 +418,8 @@ class Grid():
                 self.pipes[x,y].set_relations(self)
 
 
-    def get_max_distance(self, test=False) -> None:
-        for i in range(2):
+    def get_max_distance(self, test=False, dir = 2) -> None:
+        for i in range(dir):
             self.discover_path(i)
 
 
@@ -191,18 +436,26 @@ class Grid():
 
         cur_pipe = prev_pipe.neighbors[i]
 
+        cur_pipe.set_ori(prev_pipe)
+
         while True:
+            self.print_map()
+
             if cur_pipe.distance < 0 or cur_pipe.distance > steps:
                 cur_pipe.distance = steps
+
+            cur_pipe.set_neighbor_status(self)
     
             next_pipe = cur_pipe.get_next_pipe(prev_pipe)
 
-            if next_pipe.start:
+            if next_pipe.start or steps > MAX_ROUNDS:
+            # if next_pipe.start:
                 break
 
             steps += 1
             prev_pipe = cur_pipe
             cur_pipe = next_pipe
+            cur_pipe.set_ori(prev_pipe)
 
 
     def print_distances(self) -> None:
@@ -226,10 +479,15 @@ class Grid():
                     continue
 
                 distance = self.pipes[x,y].distance
-                if distance < 0:
-                    line += '.'
-                else:
+                status = self.pipes[x,y].status
+                if status == 'I':
+                    line += '*'
+                elif status == 'O':
+                    line += '+'
+                elif status == 'L':
                     line += UUCODE_TABLE[self.pipes[x,y].char]
+                else:
+                    line += ' '
             print(line)
 
 
@@ -254,7 +512,9 @@ def get_solution_part2(lines: list[str], test=False) -> int:
 
     grid = Grid(lines,test=test)
     grid.set_relations()
-    grid.get_max_distance(test=test)
+    grid.get_max_distance(test=test, dir=1)
+
+    grid.print_map()
 
     return [p.status for p in grid.pipes.flat].count('I')
 
