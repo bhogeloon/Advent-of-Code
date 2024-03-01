8c37c3585900d5657e2b44b14d5e1e38704ba361
Bert van Hogeloon <bert.vanhogeloon@routz.nl>
2024-03-01 16:14:41 +0100

    y2022 d01 - new format

--- Year_2022/Day_01/aoc_puzzle.py
+++ Year_2022/Day_01/aoc_puzzle.py
@@ -3,10 +3,13 @@ Year 2022, Day 1, Part 1
 
 Problem description: See https://adventofcode.com/2022/day/1
 
+<Include solution description>
+
 """
 
 # Imports
 from pprint import pprint
+
 
 # Constants
 
@@ -14,8 +17,11 @@ from pprint import pprint
 # Global variables
 
 class Gv():
-    '''Class to store global variables that are immutable, so they can be
-    reassigned within a function'''
+    '''Class to store global variables'''
+
+    # Variable that can be used to indicate we're using the test input
+    test = False
+
 
 # Classes
 
@@ -79,15 +85,44 @@ def get_fattest_elf(all_elfs: list[Elf]) -> int:
     return max_cal
 
 
-# Main function
-def get_solution(lines: list[str]) -> int:
-    '''Main function'''
+def get_fattest3_elfs(all_elfs: list[Elf]) -> int:
+    '''Return total calories of the three fattest elfs'''
+
+    max_cals = [0, 0, 0]
+
+    for elf in all_elfs:
+        cals = elf.get_cals()
+        #pprint(cals)
+
+        if cals > min(max_cals):
+            max_cals[max_cals.index(min(max_cals))] = cals
+
+    return sum(max_cals)
+
+
+# Main functions
+def get_solution_part1(lines: list[str], *args, **kwargs) -> int:
+    '''Main function for the part 1 solution'''
+
+    Gv.test = kwargs.get('test', False)
 
     all_elfs = process_input(lines)
 
     return get_fattest_elf(all_elfs)
-    
-    # return __name__
+
+    return 'part_1 ' + __name__
+
+
+def get_solution_part2(lines: list[str], *args, **kwargs) -> int:
+    '''Main function for the part 2 solution'''
+
+    Gv.test = kwargs.get('test', False)
+
+    all_elfs = process_input(lines)
+
+    return get_fattest3_elfs(all_elfs)
+
+    return 'part_2 ' + __name__
 
 
 if __name__ == '__main__':
