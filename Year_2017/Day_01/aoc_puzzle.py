c3595b9ca33f55dbd8c314def8cba469719d97d4
Bert van Hogeloon <bert.vanhogeloon@routz.nl>
2024-03-08 13:18:42 +0100

    y2017d01

--- Year_2017/Day_01/aoc_puzzle.py
+++ Year_2017/Day_01/aoc_puzzle.py
@@ -3,7 +3,16 @@ Year 2017, Day 1
 
 Problem description: See https://adventofcode.com/2017/day/1
 
-<Include solution description>
+I created the following classes:
+- Captcha: Circular list of Digit objects
+- Digit: contains a nr and a next attribute, pointing the next Digit in
+the list.
+
+In part 1, we add all numbers together if the next digit is equal.
+
+In part 2, I added an attribute 'half' to the Digit object, pointing to 
+the object on the other half of the list. I then did the same as in
+part 1, but then looking at this pointer instead.
 
 """
 
@@ -25,20 +34,55 @@ class Gv():
 
 # Classes
 
-class Captcha():
+class Captcha(list):
     '''Circular structure of Digit objects'''
     def __init__(self, line: str) -> None:
-        self.first_digit = Digit(line[0])
+        # fill the list with Digit objects
+        for char in line:
+            self.append(Digit(char))
 
-        for (i,digit) in enumerate(line):
-            pass
+        # set the circular relationships
+        for (i, digit) in enumerate(self):
+            # Set next digit relationship
+            if i == len(self) - 1:
+                digit.next = self[0]
+            else:
+                digit.next = self[i+1]
 
+            # Now set the halfway relationship
+            h = (i + len(self)//2) % len(self)
+            digit.half = self[h]
+
+
+    def solve(self) -> int:
+        '''Solve the Captcha by adding up all digits that match the
+        next digit'''
+        sum = 0
+
+        for digit in self:
+            # If nr is same as in the next digit
+            if digit.nr == digit.next.nr:
+                sum += digit.nr
+
+        return sum
+
+
+    def solve_half(self) -> int:
+        '''Solve the captcha in part 2, looking at halfway'''
+        sum = 0
+
+        for digit in self:
+            # If nr is same as in the next digit
+            if digit.nr == digit.half.nr:
+                sum += digit.nr
+
+        return sum
 
 
 class Digit():
     '''Object containing digit and next (pointer to next objec)'''
-    def __init__(self, digit: str) -> None:
-        self.digit = digit
+    def __init__(self, char: str) -> None:
+        self.nr = int(char)
 
 
 # Functions
@@ -52,6 +96,8 @@ def get_solution_part1(lines: list[str], *args, **kwargs) -> int:
 
     captcha = Captcha(lines[0])
 
+    return captcha.solve()
+
     return 'part_1 ' + __name__
 
 
@@ -59,6 +105,10 @@ def get_solution_part2(lines: list[str], *args, **kwargs) -> int:
     '''Main function for the part 2 solution'''
 
     Gv.test = kwargs.get('test', False)
+
+    captcha = Captcha(lines[0])
+
+    return captcha.solve_half()
 
     return 'part_2 ' + __name__
 
