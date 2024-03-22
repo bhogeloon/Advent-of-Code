3b445d30b154ae657f91f9440dce43af248b021f
Bert van Hogeloon <bert.vanhogeloon@routz.nl>
2024-03-22 16:22:45 +0100

    y2018d02p1

---
+++ Year_2018/Day_02/aoc_puzzle.py
@@ -0,0 +1,98 @@
+"""
+Year 2018, Day 2
+
+Problem description: See https://adventofcode.com/2018/day/2
+
+I created the following classes:
+- Box: a box with items (letters) and also a set of item_types (unique letters)
+- Boxes: Container class (list) of Box
+
+For part 1: For each box, for each item_type, check the amount of occurences, 2 times and 3 times.
+
+
+
+"""
+
+# Imports
+from pprint import pprint
+
+
+# Constants
+
+
+# Global variables
+
+class Gv():
+    '''Class to store global variables'''
+
+    # Variable that can be used to indicate we're using the test input
+    test = False
+
+
+# Classes
+
+class Box():
+    '''Candidate Box'''
+    def __init__(self, line: str) -> None:
+        self.items = line
+        # Create a set of unique letters
+        self.item_types = set(line)
+
+
+    def check_multiple_entries(self, n: int) -> bool:
+        '''Check if any letter appears n times in the items'''
+        for char in self.item_types:
+            if self.items.count(char) == n:
+                return True
+        
+        # If no match found
+        return False
+
+class Boxes(list[Box]):
+    '''Container class of Box obects'''
+    def __init__(self, lines: str) -> None:
+        for line in lines:
+            self.append(Box(line))
+
+
+    def get_checksum(self) -> int:
+        '''Get the checksum by checking all letters appearing two 
+        or three times'''
+        double_count = 0
+        triple_count = 0
+
+        for box in self:
+            if box.check_multiple_entries(2):
+                double_count += 1
+            if box.check_multiple_entries(3):
+                triple_count += 1
+
+        return double_count * triple_count
+
+
+# Functions
+
+
+# Main functions
+def get_solution_part1(lines: list[str], *args, **kwargs) -> int:
+    '''Main function for the part 1 solution'''
+
+    Gv.test = kwargs.get('test', False)
+
+    boxes = Boxes(lines)
+
+    return boxes.get_checksum()
+
+    return 'part_1 ' + __name__
+
+
+def get_solution_part2(lines: list[str], *args, **kwargs) -> int:
+    '''Main function for the part 2 solution'''
+
+    Gv.test = kwargs.get('test', False)
+
+    return 'part_2 ' + __name__
+
+
+if __name__ == '__main__':
+    pass
