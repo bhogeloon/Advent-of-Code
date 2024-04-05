ca0753e82218147bc31b9231769873ad5a36918d
Bert van Hogeloon <bert.vanhogeloon@routz.nl>
2024-04-05 15:06:16 +0200

    y2016d02p1

--- Year_2016/Day_02/aoc_puzzle.py
+++ Year_2016/Day_02/aoc_puzzle.py
@@ -3,7 +3,17 @@ Year 2016, Day 2
 
 Problem description: See https://adventofcode.com/2016/day/2
 
-<Include solution description>
+The following classes are used:
+- Keypad: contains a matrix of the keypad numbers.
+- Instruction: contains the direction of the instruction.
+- InstructionSet: Contains all the instructions leading to one digit code.
+- InstructionSets: Contains all instructionsets to retrieve the total code.
+    This class also maintains a class variable keypad which is a Keypad
+    object.
+
+For part 1: follow all the instructions and change the InstructionSets.keypad variable
+accordingly. At the end of each IntructionSet object: retrieve the current keypad
+position.
 
 """
 
@@ -43,6 +53,75 @@ class Keypad():
         self.cur_y = 1
 
 
+    def get_current(self) -> str:
+        '''Return the current key in str format'''
+        return str(self.keys[self.cur_x, self.cur_y])
+
+
+class Instruction():
+    '''Contains a single instruction (UDLR)'''
+
+    def __init__(self, char: str) -> None:
+        self.dir = char
+
+
+    def process(self) -> None:
+        '''Change the current keypad according to direction'''
+
+        keypad = InstructionSets.keypad
+
+        # If up change y if not already 0
+        if self.dir == 'U' and keypad.cur_y > 0:
+            keypad.cur_y -= 1
+        # If down change y if not already 2
+        elif self.dir == 'D' and keypad.cur_y < 2:
+            keypad.cur_y += 1
+        # If left change x if not already 0
+        elif self.dir == 'L' and keypad.cur_x > 0:
+            keypad.cur_x -= 1
+        # If right change x if not already 2
+        elif self.dir == 'R' and keypad.cur_x < 2:
+            keypad.cur_x += 1
+
+
+class InstructionSet(list[Instruction]):
+    '''Contains a list of Instruction objects which will result in 
+    a single digit.'''
+
+    def __init__(self, line: str) -> None:
+        for char in line:
+            self.append(Instruction(char))
+
+
+    def get_digit(self) -> str:
+        '''Get the digit for this line of instructions'''
+        for instr in self:
+            instr.process()
+
+        return InstructionSets.keypad.get_current()
+
+
+class InstructionSets(list[InstructionSet]):
+    '''List container class of InstructionSet objects. This will result in
+    the bathroom code'''
+
+    keypad = Keypad()
+
+    def __init__(self, lines: list[str]) -> None:
+        for line in lines:
+            self.append(InstructionSet(line))
+
+
+    def get_bathroom_code(self) -> str:
+        '''Get the solution to part 1'''
+        result = ''
+
+        for instrs in self:
+            result += instrs.get_digit()
+
+        return result
+
+
 # Functions
 
 
@@ -52,7 +131,9 @@ def get_solution_part1(lines: list[str], *args, **kwargs) -> int:
 
     Gv.test = kwargs.get('test', False)
 
-    keypad = Keypad()
+    instrs = InstructionSets(lines)
+
+    return instrs.get_bathroom_code()
 
     return 'part_1 ' + __name__
 
