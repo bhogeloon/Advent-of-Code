9a1946913be5108ca9f23afc121561c59bbc30a5
Bert van Hogeloon <bert.vanhogeloon@routz.nl>
2024-04-26 16:13:15 +0200

    y2023d02

--- Year_2023/Day_02/aoc_puzzle.py
+++ Year_2023/Day_02/aoc_puzzle.py
@@ -3,10 +3,32 @@ Year 2023, Day 2
 
 Problem description: See https://adventofcode.com/2023/day/2
 
+The following classes are used:
+- Bag: A dict class containing red, green and blue cubes
+- Turn: A game turn revealing a number of cubes of different colors. The revealed
+    cubes are put in a 'mini-bag', which is a Bag object.
+- Game: A list of Turn objects. It also has an id number.
+- Games: List container class of Game objects.
+
+For part 1:
+First we create a max-bag which is the bag which contains exactly the amount of cubes
+to compare with in each game, so we can determine which games are possible with this
+bag.
+Then we create the Games from the input, keeping track of the sum of the possible
+game-ids.
+Then we check for each game, for each turn if the total amount of balls for each colour
+is bigger than the amount in max_bag. If so, we return False. 
+
+For part 2:
+For each game, start with an empty test bag. Then go through each turn and if the amount
+of cubes for a colour is bigger than the one in the in test bag, raise the amount in the
+test bag. That will result in the minimum amount for each colour. Then return the product
+of this.
 """
 
 # Imports
 from pprint import pprint
+from math import prod
 
 # Constants
 
@@ -34,11 +56,8 @@ class Bag(dict):
 
 
     def calculate_power(self) -> int:
-        result = 1
-        for amount in self.values():
-            result *= amount
-
-        return result
+        '''Return the product of the amount of cubes for each colour'''
+        return prod(self.values())
 
 
 class Game(list):
@@ -66,10 +85,16 @@ class Game(list):
         
 
     def calculate_power(self) -> int:
+        '''Determine the minimum set of cubes and return the product
+        of those numbers'''
+
+        # Create an empty bag
         self.min_bag = Bag(0,0,0)
 
         for turn in self.turns:
             for (colour, amount) in turn.mini_bag.items():
+                # If the amount of cubes is larger than in the min_bag,
+                # Update the min_bag amount
                 if amount > self.min_bag[colour]:
                     self.min_bag[colour] = amount
 
@@ -101,7 +126,7 @@ class Turn():
         return True
 
 
-class Games(list):
+class Games(list[Game]):
     '''Collection of games'''
     
     def __init__(self, lines: list[str]) -> None:
@@ -113,6 +138,9 @@ class Games(list):
 
 
     def check_possible(self, max_bag) -> None:
+        '''Check for every game if it is possible and if so add the id
+        to the sum_of_possible_game_ids'''
+
         for game in self:
             if game.is_possible(max_bag):
                 self.sum_of_possible_game_ids += game.id
