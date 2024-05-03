1020c215eba960c8724398860cd51a70809acabc
Bert van Hogeloon <bert.vanhogeloon@routz.nl>
2024-05-03 11:32:53 +0200

    y2022d03

--- Year_2022/Day_03/aoc_puzzle.py
+++ Year_2022/Day_03/aoc_puzzle.py
@@ -3,20 +3,36 @@ Year 2022, Day 3, Part 2
 
 Problem description: See https://adventofcode.com/2022/day/3
 
+The following classes are used:
+- Rucksack: This contains sets of items for comp1 and comp2
+- Elfgroup: This is a list class containing three rucksacks (for each elf group)
+
+For part 1: get the common items for comp1 and comp2, check if it is exactly one
+and then determine the priority (based on the ASCII value).
+
+For part 2: Create an Elfgroup object for each three elves and then find the single
+common item. Then get the priority of that one.
+
 """
 
 # Imports
 from pprint import pprint
 
+
 # Constants
+
 ASCII_LOWER_BASE = ord('a') - 1
 ASCII_UPPER_BASE = ord('A') - 1
+
 
 # Global variables
 
 class Gv():
-    '''Class to store global variables that are immutable, so they can be
-    reassigned within a function'''
+    '''Class to store global variables'''
+
+    # Variable that can be used to indicate we're using the test input
+    test = False
+
 
 # Classes
 
@@ -32,6 +48,7 @@ class Rucksack():
 
         self.comp1 = set(line[:line_size//2])
         self.comp2 = set(line[line_size//2:])
+        # Get sum of all items
         self.all_comps = self.comp1 | self.comp2
         # print(line, str(self.comp1), str(self.comp2))
 
@@ -39,14 +56,18 @@ class Rucksack():
 
 
     def get_common_item(self) -> None:
+        '''Collect all common items'''
         common_items = self.comp1 & self.comp2
 
+        # If no common items found
         if len(common_items) == 0:
             raise RuntimeError('No common items found: {}'.format(self.line))
 
+        # If more than one common item found
         if len(common_items) > 1:
             raise RuntimeError('More than one common item found: {}.'.format(self.line))
 
+        # Extract the one common item
         self.common_item = common_items.pop()
 
         return
@@ -55,16 +76,14 @@ class Rucksack():
     def get_priority(self) -> int:
         '''get the priority of the common item'''
 
-        if self.common_item.isupper():
-            return ord(self.common_item) - ASCII_UPPER_BASE + 26
-        else:
-            return ord(self.common_item) - ASCII_LOWER_BASE
+        return get_item_priority(self.common_item)
 
 
 class ElfGroup(list[Rucksack]):
     '''Group of 3 rucksacks'''
 
     def get_common_item(self) -> None:
+        '''Get the common item of all three rucksacks'''
         common_items = self[0].all_comps & self[1].all_comps & self[2].all_comps
 
         if len(common_items) == 0:
@@ -81,15 +100,42 @@ class ElfGroup(list[Rucksack]):
     def get_priority(self) -> int:
         '''get the priority of the common item'''
 
-        if self.common_item.isupper():
-            return ord(self.common_item) - ASCII_UPPER_BASE + 26
-        else:
-            return ord(self.common_item) - ASCII_LOWER_BASE
+        return get_item_priority(self.common_item)
 
 
-# Main function
-def get_solution(lines: list[str]) -> int:
-    '''Main function'''
+# Functions
+
+def get_item_priority(char: str) -> int:
+    '''function to get prio of a character'''
+
+    if char.isupper():
+        return ord(char) - ASCII_UPPER_BASE + 26
+    else:
+        return ord(char) - ASCII_LOWER_BASE
+
+
+# Main functions
+def get_solution_part1(lines: list[str], *args, **kwargs) -> int:
+    '''Main function for the part 1 solution'''
+
+    Gv.test = kwargs.get('test', False)
+
+    prio_sum = 0
+
+    for line in lines:
+        rucksack = Rucksack(line)
+        rucksack.get_common_item()
+        prio_sum += rucksack.get_priority()
+
+    return prio_sum
+
+    return 'part_1 ' + __name__
+
+
+def get_solution_part2(lines: list[str], *args, **kwargs) -> int:
+    '''Main function for the part 2 solution'''
+
+    Gv.test = kwargs.get('test', False)
 
     prio_sum = 0
 
@@ -104,7 +150,7 @@ def get_solution(lines: list[str]) -> int:
 
     return prio_sum
 
-    # return __name__
+    return 'part_2 ' + __name__
 
 
 if __name__ == '__main__':
