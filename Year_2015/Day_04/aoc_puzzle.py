e1b712ecfd067880feaf59f6ae585f8b31b19e77
Bert van Hogeloon <bert.hogeloon@kpn.com>
2024-06-21 15:36:11 +0200

    y2015d04p2

--- Year_2015/Day_04/aoc_puzzle.py
+++ Year_2015/Day_04/aoc_puzzle.py
@@ -9,6 +9,8 @@ The following class is used:
 Part 1: Go over all integers starting with 1, extend the key with the integer and calculate
 the MD5 key using the Python haslib until you encounter a string starting with 5 leading 0's.
 
+Part 2: Now do the same with 6 zeros. No problem. Takes almost 3 seconds.
+
 """
 
 # Imports
@@ -36,10 +38,11 @@ class SecretKey():
         self.key = line
 
 
-    def mine_coin(self) -> int:
+    def mine_coin(self, zeros=5) -> int:
         '''Get the lowest number where the MD5 hash for the combined string starts
-        with 5 leading 0's'''
+        with zeros amount of leading 0's'''
         result = 0
+        zeros_str = '0' * zeros
 
         while True:
             result += 1
@@ -47,7 +50,7 @@ class SecretKey():
             md5_obj = hashlib.md5(comb_str.encode())
             md_hash_str = md5_obj.hexdigest()
 
-            if md_hash_str[:5] == '00000':
+            if md_hash_str[:zeros] == zeros_str:
                 return result
 
 
@@ -71,6 +74,10 @@ def get_solution_part2(lines: list[str], *args, **kwargs) -> int:
     '''Main function for the part 2 solution'''
 
     Gv.test = kwargs.get('test', False)
+
+    secret_key = SecretKey(lines[0])
+
+    return secret_key.mine_coin(zeros=6)
 
     return 'part_2 ' + __name__
 
