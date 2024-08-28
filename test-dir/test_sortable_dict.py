a2a0c922c82ff7fff961f39590fe2d0c505fdc46
Bert van Hogeloon <bert.vanhogeloon@routz.nl>
2024-08-28 16:30:01 +0200

    Set up unit test env

--- test-dir/test_sortable_dict.py
+++ test-dir/test_sortable_dict.py
@@ -5,6 +5,27 @@ Test modudule to test the functions in the lib.sortable_dict module
 import unittest
 from aoc_lib.sortable_dict import SortableDict
 
+# Test data
+
+test_data_normal_json = """
+[
+    {
+        'in': {
+            'a': 4,
+            'd': 2,
+            'c': 1,
+            'b': 2,
+        }
+        'out': {
+            'c': 1,
+            'b': 2,
+            'd': 2,
+            'a': 4,
+        }
+    }
+]
+"""
+
 class SortableDictTest(unittest.TestCase):
     def test_test(self):
         self.assertTrue(True)
