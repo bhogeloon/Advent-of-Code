5a5fc73568d0d6c2e6f37484ee09bff9810661ed
Bert van Hogeloon <bert.vanhogeloon@routz.nl>
2024-08-28 12:12:30 +0200

    Set up unit tests

---
+++ test_sortable_dict.py
@@ -0,0 +1,14 @@
+"""
+Test modudule to test the functions in the lib.sortable_dict module
+"""
+
+import unittest
+from aoc_lib.sortable_dict import SortableDict
+
+class SortableDictTest(unittest.TestCase):
+    def test_test(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
