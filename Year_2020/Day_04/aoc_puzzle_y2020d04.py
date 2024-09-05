f50654aa93cacf374d4f804830a44c1fd4977213
Bert van Hogeloon <bert.vanhogeloon@routz.nl>
2024-09-05 16:31:23 +0200

    y2020d04p2

--- Year_2020/Day_04/aoc_puzzle_y2020d04.py
+++ Year_2020/Day_04/aoc_puzzle_y2020d04.py
@@ -8,6 +8,9 @@ We have the following classes:
 - Passports: List container class of Passport objects
 
 Part 1: Look at all the keys and see if all the mandatory keys are in it.
+
+Part 2: First do the part 1 check and then check the specific attributes one
+by one.
 """
 
 # Imports
@@ -47,18 +50,87 @@ class Passport(dict):
         'cid',
     }
 
+    ECL_VALS = (
+        'amb',
+        'blu',
+        'brn',
+        'gry',
+        'grn',
+        'hzl',
+        'oth',
+    )
+   
+
     def __init__(self) -> None:
         pass
 
 
-    def is_valid(self) -> bool:
-        '''Checks if the passport is valid'''
+    def is_valid_p1(self) -> bool:
+        '''Checks if the passport is valid (according the part 1 rules)'''
         # Turn the keys into a set
         pp_keys = set(self.keys())
 
         # Check if all the mandatory keys are in the key_set
         if self.mandatory_fields.issubset(pp_keys):
             return True
+        else:
+            return False
+
+
+    def is_valid_p2(self) -> bool:
+        '''Checks if the passport is valid (according the part 2 rules)'''
+        # Check if all mandatory fields are actually there
+        if not self.is_valid_p1():
+            return False
+        
+        # Check byr
+        if not self.check_year(self['byr'], 1920, 2002):
+            return False
+
+        # Check iyr
+        if not self.check_year(self['iyr'], 2010, 2020):
+            return False
+
+        # Check eyr
+        if not self.check_year(self['eyr'], 2020, 2030):
+            return False
+
+        # Check hgt
+        if re.fullmatch(r'\d+cm', self['hgt']):
+            hgt_int = int(self['hgt'][:-2])
+            if hgt_int < 150 or hgt_int > 193:
+                return False
+        elif re.fullmatch(r'\d+in', self['hgt']):
+            hgt_int = int(self['hgt'][:-2])
+            if hgt_int < 59 or hgt_int > 76:
+                return False
+        else:
+            return False
+
+        # Check hcl
+        if not re.fullmatch(r'#[0-9a-f]{6}', self['hcl']):
+            return False
+
+        # Check ecl
+        if self['ecl'] not in self.ECL_VALS:
+            return False
+
+        # Check pid
+        if not re.fullmatch(r'\d{9}', self['pid']):
+            return False
+
+        return True
+
+
+    def check_year(self, year_str: str, min_val: int, max_val: int) -> bool:
+        '''Checks if the year format is correct and in between the values'''
+
+        if re.fullmatch('\d{4}', year_str):
+            year_int = int(year_str)
+            if year_int < min_val or year_int > max_val:
+                return False
+            else:
+                return True
         else:
             return False
 
@@ -89,15 +161,27 @@ class Passports(list[Passport]):
                 self.append(current_pp)
 
 
-    def get_valid_nr(self) -> int:
-        '''Get the number of valid passports'''
+    def get_valid_nr_p1(self) -> int:
+        '''Get the number of valid passports (according the part 1 rules)'''
         result = 0
 
         for passport in self:
-            if passport.is_valid():
+            if passport.is_valid_p1():
                 result += 1
 
         return result
+
+
+    def get_valid_nr_p2(self) -> int:
+        '''Get the number of valid passports (according the part 2 rules)'''
+        result = 0
+
+        for passport in self:
+            if passport.is_valid_p2():
+                result += 1
+
+        return result
+
 
 # Functions
 
@@ -110,7 +194,7 @@ def get_solution_part1(lines: list[str], *args, **kwargs) -> int:
 
     passports = Passports(lines)
 
-    return passports.get_valid_nr()
+    return passports.get_valid_nr_p1()
 
     return 'part_1 ' + __name__
 
@@ -119,6 +203,10 @@ def get_solution_part2(lines: list[str], *args, **kwargs) -> int:
     '''Main function for the part 2 solution'''
 
     Gv.test = kwargs.get('test', False)
+
+    passports = Passports(lines)
+
+    return passports.get_valid_nr_p2()
 
     return 'part_2 ' + __name__
 
