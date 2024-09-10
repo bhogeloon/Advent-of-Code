a866c54f490aeeac418e80f25b461952525d2cc1
Bert van Hogeloon <bert.vanhogeloon@routz.nl>
2024-09-10 14:21:15 +0200

    y2015d05p2

--- Year_2015/Day_05/aoc_puzzle_y2015d05.py
+++ Year_2015/Day_05/aoc_puzzle_y2015d05.py
@@ -9,6 +9,16 @@ The following classes are used:
 
 Part 1: Run the three checks on each string and count the nice ones.
 
+Part 2:
+- For the first check, divice the string each time in three sections:
+    - The substring of two characters (iterating over the whole string)
+    - The remainder before the substring
+    - The remainder after the substring
+    If the substring is part of either remainder, the check is valid.
+- For the second check, consider a group of three characters through
+    the string. Then compare the first and last character of that group
+    of three.
+
 """
 
 # Imports
@@ -44,13 +54,19 @@ class SantaString(str):
     )
 
 
-    def is_nice(self) -> bool:
+    def is_nice(self, part=1) -> bool:
         '''Check if the string is nice or naughty'''
-        return (
-            self.has_3_vowels() and 
-            self.check_double() and 
-            self.check_allowed_substr()
-        )
+        if part == 1:
+            return (
+                self.has_3_vowels() and 
+                self.check_double() and 
+                self.check_allowed_substr()
+            )
+        else:
+            return (
+                self.has_repeating_pair() and
+                self.repeating_char_with_1_between()
+            )
     
 
     def has_3_vowels(self) -> bool:
@@ -83,6 +99,33 @@ class SantaString(str):
                 return False
             
         return True
+    
+
+    def has_repeating_pair(self) -> bool:
+        '''Returns True if it has a repeating, non-overlapping pair'''
+        # Repeat for all character pairs
+        for i in range(len(self) - 1):
+            substring = self[i:i+2]
+            remainder1 = self[:i]
+            remainder2 = self[i+2:]
+
+            if substring in remainder1 or substring in remainder2:
+                return True
+
+        return False
+    
+
+    def repeating_char_with_1_between(self) -> bool:
+        '''Returns True if it finds a character that repeats itself with
+        one character in between'''
+        # Repeat for all character trios
+        for i in range(len(self) - 2):
+ 
+            # If character in i equals the character two places away
+            if self[i] == self[i+2]:
+                return True
+
+        return False
 
 
 class TextFile(list[SantaString]):
@@ -92,12 +135,12 @@ class TextFile(list[SantaString]):
             self.append(SantaString(line))
 
 
-    def get_nr_of_nice(self) -> int:
+    def get_nr_of_nice(self, part=1) -> int:
         '''Return the number of nice strings'''
         nr_of_nice = 0
 
         for santastring in self:
-            if santastring.is_nice():
+            if santastring.is_nice(part=part):
                 nr_of_nice += 1
 
         return nr_of_nice
@@ -123,6 +166,10 @@ def get_solution_part2(lines: list[str], *args, **kwargs) -> int:
     '''Main function for the part 2 solution'''
 
     Gv.test = kwargs.get('test', False)
+
+    text_file = TextFile(lines)
+
+    return text_file.get_nr_of_nice(part=2)
 
     return 'part_2 ' + __name__
 
