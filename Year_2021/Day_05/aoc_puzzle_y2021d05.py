c965f43f2879d5762951d0fe50c9ae63d0a57f0e
Bert van Hogeloon <bert.vanhogeloon@routz.nl>
2024-09-12 10:59:58 +0200

    y2021d05p2

--- Year_2021/Day_05/aoc_puzzle_y2021d05.py
+++ Year_2021/Day_05/aoc_puzzle_y2021d05.py
@@ -13,6 +13,13 @@ Part 1: In processing the VentLines, we only consider VentLines it the
 x coordinates or the y coordinates are equal. Then we walk through the
 other coordinate to increase the number in the grid.
 
+Part 2: For considering the diagonal line, we do it in three steps:
+- First we calculate the total range of steps, but just looking at 
+    one coordinate (as both use the same range as they are 45 degrees)
+- Then we look at the direction each coordinate is going to determine 
+    whether we need to count up or down.
+- Then we start the for loop to update the grid.
+
 """
 
 # Imports
@@ -64,25 +71,59 @@ class OceanFloor(Grid2D):
         )
 
 
-    def process_vent_lines_straight(self, vent_lines: VentLines) -> None:
+    def process_vent_lines(self, vent_lines: VentLines, straight=True) -> None:
         '''Processs all the ventlines and update the grid, using only straight
         lines'''
         for vent_line in vent_lines:
             # if x coordinates are the same:
             if vent_line.x1 == vent_line.x2:
-                # Put them in the right order
-                (y_small, y_big) = sorted([vent_line.y1, vent_line.y2])
-                for y in range(y_small, y_big + 1):
-                    self.grid[vent_line.x1, y] += 1
+                self.process_vert_line(vent_line)
 
             # if y coordinates are the same:
             elif vent_line.y1 == vent_line.y2:
-                # Put them in the right order
-                (x_small, x_big) = sorted([vent_line.x1, vent_line.x2])
-                for x in range(x_small, x_big + 1):
-                    self.grid[x, vent_line.y1] += 1
+                self.process_hor_line(vent_line)
 
             # Otherwise it is not a straight line, so no further action needed
+            elif not straight:
+                self.process_diag_line(vent_line)
+
+
+    def process_vert_line(self, vent_line: VentLine) -> None:
+        '''Process a vertical line (where x coordinates are the same)'''
+        # Put them in the right order
+        (y_small, y_big) = sorted([vent_line.y1, vent_line.y2])
+        for y in range(y_small, y_big + 1):
+            self.grid[vent_line.x1, y] += 1
+
+
+    def process_hor_line(self, vent_line: VentLine) -> None:
+        '''Process a horizontal line (where y coordinates are the same)'''
+        # Put them in the right order
+        (x_small, x_big) = sorted([vent_line.x1, vent_line.x2])
+        for x in range(x_small, x_big + 1):
+            self.grid[x, vent_line.y1] += 1
+
+
+    def process_diag_line(self, vent_line: VentLine) -> None:
+        '''Process a diagonal line'''
+        # Determine  absolute range
+        abs_range = range(abs(vent_line.x2-vent_line.x1)+1)
+
+        # Determine x step
+        if vent_line.x1 < vent_line.x2:
+            x_step = 1
+        else:
+            x_step = -1
+
+        # Determine y-range
+        if vent_line.y1 < vent_line.y2:
+            y_step = 1
+        else:
+            y_step = -1
+
+        # Update matrix
+        for i in abs_range:
+            self.grid[vent_line.x1+i*x_step, vent_line.y1+i*y_step] += 1
 
 
     def get_nr_of_crossings(self) -> int:
@@ -107,7 +148,7 @@ def get_solution_part1(lines: list[str], *args, **kwargs) -> int:
 
     floor = OceanFloor()
     vent_lines = VentLines(lines)
-    floor.process_vent_lines_straight(vent_lines)
+    floor.process_vent_lines(vent_lines)
 
     return floor.get_nr_of_crossings()
 
@@ -118,6 +159,12 @@ def get_solution_part2(lines: list[str], *args, **kwargs) -> int:
     '''Main function for the part 2 solution'''
 
     Gv.test = kwargs.get('test', False)
+
+    floor = OceanFloor()
+    vent_lines = VentLines(lines)
+    floor.process_vent_lines(vent_lines, straight=False)
+
+    return floor.get_nr_of_crossings()
 
     return 'part_2 ' + __name__
 
