6a5b019187420a82fde3946022394d00cb3a55d0
Bert van Hogeloon <bert.vanhogeloon@routz.nl>
2024-09-20 14:27:30 +0200

    y2020d06p2

--- Year_2020/Day_06/aoc_puzzle_y2020d06.py
+++ Year_2020/Day_06/aoc_puzzle_y2020d06.py
@@ -8,7 +8,11 @@ The following classes are used:
     answers (set), which contains all catagories answered with "yes".
 - PassengerGroups: List container class of PassengerGroup objects.
 
-Part 1: Summarise the length of all answers attribute.
+Part 1: Use logical or to merge answer sets. Summarise the length of all answers 
+attribute.
+
+Part 2: For the first person, fill the set and for all next person, use logical 
+and to merge the answer sets. Summarise in the end.
 
 """
 
@@ -33,33 +37,57 @@ class Gv():
 class PassengerGroup():
     '''Represents a group of passengers. They have an attribute answers (set),
     which contains all catagories answered with "yes".'''
-    def __init__(self, answers: set[str]) -> None:
-        self.answers = answers
+    def __init__(self) -> None:
+        # Contains the set of cats that anyone has answered
+        self.anyone_yes = set()
+        # Contains the set of cats that everyone has answered
+        # Starts with None to indicate that the first time it needs
+        # to be set with the full contents of the persons answers
+        self.all_yes = None
+
+
+    def add_person(self, line: str) -> None:
+        '''Add the answers from one person in the group'''
+        self.anyone_yes |= set(line)
+
+        if self.all_yes == None:
+            self.all_yes = set(line)
+        else:
+            self.all_yes &= set(line)
 
 
 class PassengerGroups(list[PassengerGroup]):
     '''List container class of PassengerGroup objects'''
     def __init__(self, lines: list[str]) -> None:
-        answers = set()
+        new_pgroup = PassengerGroup()
+        self.append(new_pgroup)
 
         for (i, line) in enumerate(lines):
             if line == '':
-                self.append(PassengerGroup(answers))
-                answers = set()
-                continue
-
-            answers |= set(line)
-
-            if len(lines) == i+1:
-                self.append(PassengerGroup(answers))
+                new_pgroup = PassengerGroup()
+                self.append(new_pgroup)
+            else:
+                new_pgroup.add_person(line)
 
 
-    def get_all_answers(self) -> int:
-        '''Get the sum of all answers of all groups'''
+    def get_all_answers_any_yes(self) -> int:
+        '''Get the sum of all categories of all groups where anyone has
+        answered yes'''
         sum_of_answers = 0
 
         for pgroup in self:
-            sum_of_answers += len(pgroup.answers)
+            sum_of_answers += len(pgroup.anyone_yes)
+
+        return sum_of_answers
+
+
+    def get_all_answers_all_yes(self) -> int:
+        '''Get the sum of all categories of all groups where everyone has
+        answered yes'''
+        sum_of_answers = 0
+
+        for pgroup in self:
+            sum_of_answers += len(pgroup.all_yes)
 
         return sum_of_answers
 
@@ -75,7 +103,7 @@ def get_solution_part1(lines: list[str], *args, **kwargs) -> int:
 
     pgroups = PassengerGroups(lines)
 
-    return pgroups.get_all_answers()
+    return pgroups.get_all_answers_any_yes()
 
     return 'part_1 ' + __name__
 
@@ -84,6 +112,10 @@ def get_solution_part2(lines: list[str], *args, **kwargs) -> int:
     '''Main function for the part 2 solution'''
 
     Gv.test = kwargs.get('test', False)
+
+    pgroups = PassengerGroups(lines)
+
+    return pgroups.get_all_answers_all_yes()
 
     return 'part_2 ' + __name__
 
