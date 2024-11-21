4fb087564409c23201cd583592b1a8250decf50f
Bert van Hogeloon <bert.vanhogeloon@routz.nl>
2024-11-21 16:00:30 +0100

    y2015d08p2

--- Year_2015/Day_08/aoc_puzzle_y2015d08.py
+++ Year_2015/Day_08/aoc_puzzle_y2015d08.py
@@ -10,6 +10,13 @@ The following classes are used:
 Part 1: Use the python eval() function to evaluate the string in each line.
 Then get the difference in length between the line (SantaLine.code) and the
 evaluated line (SantaLine.mem). Add those together for all lines.
+
+Part 2: Do three operations on each code line:
+- Replace all \ with \\.
+- Replace all " with \".
+- Put a " before and after the string.
+Then calculate all the length differences.
+
 """
 
 # Imports
@@ -45,7 +52,16 @@ class SantaLine:
         self.code = line
         self.mem = eval(self.code)
 
-        Gv.log.debug(f'{self.code} --- {self.mem}')
+        self.expand_code()
+
+        Gv.log.debug(f'{self.code} --- {self.mem} --- {self.exp_code}')
+
+
+    def expand_code(self) -> None:
+        '''Exand the code attribute to escape all special characters'''
+        self.exp_code = self.code.replace('\\', '\\\\')
+        self.exp_code = self.exp_code.replace('"', '\\"')
+        self.exp_code = '"' + self.exp_code + '"'
 
 
 class SantaList(list[SantaLine]):
@@ -62,6 +78,17 @@ class SantaList(list[SantaLine]):
 
         for santaline in self:
             total += len(santaline.code) - len(santaline.mem)
+
+        return total
+
+
+    def get_expcode_minus_code(self) -> int:
+        '''Get the total amount of expanced  code characters minus the total 
+        amount of code characters'''
+        total = 0
+
+        for santaline in self:
+            total += len(santaline.exp_code) - len(santaline.code)
 
         return total
 
@@ -87,6 +114,10 @@ def get_solution_part2(lines: list[str], *args, **kwargs) -> int:
     '''Main function for the part 2 solution'''
 
     Gv(**kwargs)
+
+    santalist = SantaList(lines)
+
+    return santalist.get_expcode_minus_code()
 
     return 'part_2 ' + __name__
 
