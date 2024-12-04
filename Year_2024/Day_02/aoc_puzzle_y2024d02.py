64b90f6b89132c73b6919511b351483e73aadc24
Bert van Hogeloon <bert.vanhogeloon@routz.nl>
2024-12-04 16:24:24 +0100

    y2024d02p2

--- Year_2024/Day_02/aoc_puzzle_y2024d02.py
+++ Year_2024/Day_02/aoc_puzzle_y2024d02.py
@@ -12,12 +12,17 @@ size of the difference. If the order is not yet known record it. If it is known
 check if it stays that way.
 Count the number of safe reports.
 
+Part 2: Check whether the report is safe (as in part 1), but if not: copy the
+report, remove one level and check again. Do this for each level. If one is
+safe, the whole report is safe.
+
 """
 
 # Imports
 from __future__ import annotations
 from pprint import pprint
 from logging import Logger
+from copy import deepcopy
 
 # Constants
 
@@ -45,7 +50,6 @@ class Report(list[int]):
     '''List class containing several levels'''
     def __init__(self, line: str) -> None:
         self.extend([int(lvl) for lvl in line.split()])
-        Gv.log.debug(self)
 
         # Order can be 'asc', 'dec' or 'unk'(own)
         self.order = 'unk'
@@ -80,6 +84,28 @@ class Report(list[int]):
 
         # If not bailed out, report is safe
         return True
+    
+
+    def is_dampened_safe(self) -> bool:
+        '''Check if the report is safe, considering the Problem Dampener'''
+        # First, if the whole report is safe, we are good
+        if self.is_safe():
+            return True
+        
+        # Now remove a level one by one and check again
+        for i in range(len(self)):
+            dampened_report = deepcopy(self)
+            # Set the order back to unknown
+            dampened_report.order = 'unk'
+            del dampened_report[i]
+            # Gv.log.debug(dampened_report)
+
+            if dampened_report.is_safe():
+                return True
+            
+        # If none are safe
+        # Gv.log.debug(self)
+        return False
 
 
 class UnusualData(list[Report]):
@@ -95,6 +121,18 @@ class UnusualData(list[Report]):
 
         for report in self:
             if report.is_safe():
+                nr += 1
+
+        return nr
+    
+
+    def nr_of_dampened_safe_reports(self) -> int:
+        '''Return the number of safer reports, considering the Problem
+        Dampener'''
+        nr = 0
+
+        for report in self:
+            if report.is_dampened_safe():
                 nr += 1
 
         return nr
@@ -120,6 +158,10 @@ def get_solution_part2(lines: list[str], *args, **kwargs) -> int:
     '''Main function for the part 2 solution'''
 
     Gv(**kwargs)
+
+    data = UnusualData(lines)
+
+    return data.nr_of_dampened_safe_reports()
 
     return 'part_2 ' + __name__
 
