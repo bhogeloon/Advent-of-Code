1ae10c690a520abd9cffdb6abf06bd9254e3c4e0
Bert van Hogeloon <bert.vanhogeloon@routz.nl>
2024-12-19 13:47:16 +0100

    y2024d05p2

--- Year_2024/Day_05/aoc_puzzle_y2024d05.py
+++ Year_2024/Day_05/aoc_puzzle_y2024d05.py
@@ -13,7 +13,14 @@ Part 1: For each Update, check for each rule if the index for the before page
 is smaller than the index for the after page. If the rule contains a page number
 that is not part of the update, ignore the rule. If all rules comply, this
 update is correct. Then determine the middle number and add those up.
- 
+
+Part 2: Now in addition to check if the update complies to each rule, I 
+immediately correct the update by moving the 'before' value just before the
+'after' value. I also proceed with checking the result and repeat going over
+all the rules until no issues have been found anymore.
+Then finally consider only the Updates that were corrected and get the middle
+value and add them up.
+
 """
 
 # Imports
@@ -52,17 +59,27 @@ class Rule:
         self.after = int(elements[1])
 
 
-    def comply(self, update: Update) -> bool:
-        '''Check if the update complies to the Rule'''
+    def comply(self, update: Update, correct=False) -> bool:
+        '''Check if the update complies to the Rule. If it does not comply and 
+        the correct flag is set, correct the update, but still return False.'''
         # If the before or after is not part of the update, the rule does not
         # apply
         if self.before not in update or self.after not in update:
             return True
         
-        if update.index(self.before) < update.index(self.after):
+        before_index = update.index(self.before)
+        after_index = update.index(self.after)
+
+        if before_index < after_index:
             return True
-        else:
-            return False
+
+        if correct:
+            # Move the before value just before the after value
+            del update[before_index]
+            update.insert(after_index, self.before)
+
+        return False
+        
 
 
 class Rules(list[Rule]):
@@ -79,19 +96,33 @@ class Update(list[int]):
         self.extend(pages)
 
 
-    def is_correct(self, rules: Rules) -> bool:
-        '''Check if the update is according to the rules'''
-        for rule in rules:
-            if not rule.comply(self):
-                return False
+    def is_correct(self, rules: Rules, correct=False) -> bool:
+        '''Check if the update is according to the rules. If it is not
+        correct and the correct flag is set, correct it, but still return 
+        False.'''
+        result = True
 
-        return True
+        found_incorrect = True
+
+        # Repeat the process until no issues found
+        while found_incorrect:
+            found_incorrect = False
+
+            for rule in rules:
+                if not rule.comply(self, correct=correct):
+                    if correct:
+                        result = False
+                        found_incorrect = True
+                    else:
+                        return False
+
+        return result
     
 
     def get_middle_page(self) -> int:
         '''Return the middle page'''
         return self[len(self)//2]
-
+    
 
 class Updates(list[Update]):
     '''List container class of Update objects'''
@@ -100,12 +131,22 @@ class Updates(list[Update]):
             self.append(Update(line))
 
 
-    def get_middle_pages(self, rules: Rules) -> int:
+    def get_middle_pages(self, rules: Rules, part=1) -> int:
         '''Get the sum of all middle pages of each update, if correct'''
         result = 0
 
+        if part == 1:
+            correct = False
+        else:
+            correct = True
+
         for update in self:
-            if update.is_correct(rules):
+            update_is_correct = update.is_correct(rules, correct=correct)
+
+            if part == 1 and update_is_correct:
+                result += update.get_middle_page()
+            elif part == 2 and not update_is_correct:
+                Gv.log.debug(f'Getting middle page for {update}')
                 result += update.get_middle_page()
 
         return result
@@ -156,6 +197,10 @@ def get_solution_part2(lines: list[str], *args, **kwargs) -> int:
     '''Main function for the part 2 solution'''
 
     Gv(**kwargs)
+
+    rules, updates = process_input(deque(lines))
+
+    return updates.get_middle_pages(rules,part=2)
 
     return 'part_2 ' + __name__
 
