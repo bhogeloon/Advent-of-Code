df868f85ea5e4a0e1f9a16fa8f92b2ea9a77b235
Bert van Hogeloon <bert.vanhogeloon@routz.nl>
2025-04-29 15:11:41 +0200

    y2024d08p2

--- Year_2024/Day_08/aoc_puzzle_y2024d08.py
+++ Year_2024/Day_08/aoc_puzzle_y2024d08.py
@@ -14,6 +14,10 @@ Part 1: Go through the Frequencies dict and for each frequence extrapolate
 each location pair in both directions. Flag those locations as antinode.
 Then count the antinode locations.
 
+Part 2: Extrapolate the location pair until you go off the grid. It was a 
+little unclear that in this case also the antenna position themselves are
+counted, but once I got that, I got the right result.
+
 """
 
 # Imports
@@ -53,7 +57,7 @@ class Location():
             self.freq = freq
 
         # Indicates whether an antinode will be present
-        self.antinode = 0
+        self.antinode = False
 
 
 class Frequencies(dict[str,list[Location]]):
@@ -88,7 +92,8 @@ class Map(Grid2D):
         result = 0
 
         for loc in self.grid.flat:
-            result += loc.antinode
+            if loc.antinode:
+                result += 1
 
         return result
 
@@ -100,10 +105,6 @@ class Map(Grid2D):
             if len(antennas) == 1:
                 continue
 
-            # List of coordinates (tuples) that need to contain
-            # an antinode list
-            antinode_coords = []
-
             # Now calculate for each pair
             for i in range(len(antennas)-1):
                 for j in range(i+1,len(antennas)):
@@ -112,18 +113,22 @@ class Map(Grid2D):
                         f"{antennas[i].y}), ({antennas[j].x},"
                         f"{antennas[j].y}), freq: {freq}")
                     if part == 1:
-                        antinode_coords.extend(self.get_new_coord(
+                        self.get_new_coord(
                             (antennas[i].x, antennas[i].y),
                             (antennas[j].x, antennas[j].y)
-                        ))
+                        )
                     else:
-                        antinode_coords.extend(self.get_new_coord2(
+                        self.get_new_coord2(
                             (antennas[i].x, antennas[i].y),
                             (antennas[j].x, antennas[j].y)
-                        ))
+                        )
+
+                        # Now also set the antinode for the antennas themselves
+                        antennas[i].antinode = True
+                        antennas[j].antinode = True
 
 
-    def get_new_coord(self, c1:tuple[int], c2: tuple[int]) -> list[tuple[int]]:
+    def get_new_coord(self, c1:tuple[int], c2: tuple[int]) -> None:
         '''Extrapolate the 2 points on both sides and return a list
         of new coordinates'''
         result = []
@@ -161,9 +166,8 @@ class Map(Grid2D):
     def store_coords(self, coords: tuple[int]) -> None:
         '''Store the coords if they are within the grid'''
         if self.is_in_grid(coords):
-            self.grid[coords[0],coords[1]].antinode += 1
-            Gv.log.debug(f"Increasing antinode for ({coords[0]},{coords[1]}) "
-                f"to {self.grid[coords[0],coords[1]].antinode}")
+            self.grid[coords[0],coords[1]].antinode = True
+            Gv.log.debug(f"Setting antinode for ({coords[0]},{coords[1]})")
 
 
     def get_new_coord2(self, c1:tuple[int], c2: tuple[int]) -> list[tuple[int]]:
@@ -180,16 +184,14 @@ class Map(Grid2D):
             cur_y = min(y1,y2)
             while cur_y >= 0:
                 new_y = cur_y - abs(y1-y2)
-                if new_y >= 0:
-                    result.append((new_x,new_y))
+                self.store_coords((new_x,new_y))
                 cur_y = new_y
 
             # extend down
             cur_y = max(y1,y2)
             while cur_y < self.size_y:
                 new_y = cur_y + abs(y1-y2)
-                if new_y < self.size_y:
-                    result.append((new_x,new_y))
+                self.store_coords((new_x,new_y))
                 cur_y = new_y
                 
         elif y1 == y2:
@@ -198,16 +200,14 @@ class Map(Grid2D):
             cur_x = min(x1,x2)
             while cur_x >= 0:
                 new_x = cur_x - abs(x1-x2)
-                if new_x >= 0:
-                    result.append((new_x,new_y))
+                self.store_coords((new_x,new_y))
                 cur_x = new_x
 
             # extend to right
             cur_x = max(x1,x2)
             while cur_x < self.size_x:
                 new_x = cur_x + abs(x1-x2)
-                if new_x < self.size_x:
-                    result.append((new_x,new_y))
+                self.store_coords((new_x,new_y))
                 cur_x = new_x
 
         # Extend left up to right down
@@ -218,8 +218,7 @@ class Map(Grid2D):
             while cur_x >= 0 and cur_y >=0:
                 new_x = cur_x - abs(x1-x2)
                 new_y = cur_y - abs(y1-y2)
-                if new_x >= 0 and new_y >=0:
-                    result.append((new_x,new_y))
+                self.store_coords((new_x,new_y))
                 cur_x = new_x
                 cur_y = new_y
 
@@ -229,8 +228,7 @@ class Map(Grid2D):
             while cur_x < self.size_x and cur_y < self.size_y:
                 new_x = cur_x + abs(x1-x2)
                 new_y = cur_y + abs(y1-y2)
-                if new_x < self.size_x and new_y < self.size_y:
-                    result.append((new_x,new_y))
+                self.store_coords((new_x,new_y))
                 cur_x = new_x
                 cur_y = new_y
 
@@ -242,8 +240,7 @@ class Map(Grid2D):
             while cur_x >= 0 and cur_y < self.size_y:
                 new_x = cur_x - abs(x1-x2)
                 new_y = cur_y + abs(y1-y2)
-                if new_x >= 0 and new_y < self.size_y:
-                    result.append((new_x,new_y))
+                self.store_coords((new_x,new_y))
                 cur_x = new_x
                 cur_y = new_y
 
@@ -253,8 +250,7 @@ class Map(Grid2D):
             while cur_x < self.size_x and cur_y >= 0:
                 new_x = cur_x + abs(x1-x2)
                 new_y = cur_y - abs(y1-y2)
-                if new_x < self.size_x and new_y >= 0:
-                    result.append((new_x,new_y))
+                self.store_coords((new_x,new_y))
                 cur_x = new_x
                 cur_y = new_y
 
@@ -268,7 +264,7 @@ class Map(Grid2D):
         for y in range(self.sizes[1]):
             for x in range(self.sizes[0]):
                 if self.grid[x,y].freq is None:
-                    if self.grid[x,y].antinode > 0:
+                    if self.grid[x,y].antinode:
                         result += '#'
                     else:
                         result += '.'
